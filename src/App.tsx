36d48d20aaff81420f3d7c6d24db2142dc65f82f
Scott Dawson <github@dawsonmediadesign.com>
2020-06-20 15:46:10 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import './App.css';
+import ReactSlider from 'react-slider'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+import { faTwitter, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { 
+  faHeart,
+  faBomb,
+  faDove
+} from '@fortawesome/free-solid-svg-icons'
+import { sounds, backgrounds, twitterShareLink, facebookShareLink } from './Constants'
+
+interface Track {
+  title: string,
+  src: string,
+  icon: IconDefinition,
+  isPlaying?: boolean,
+  player?: any,
+}
+
+interface AppState {
+  trackList: Track[]
+}
+
+interface MyProps {}
+
+class App extends React.Component<MyProps, AppState> {
+  
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      trackList: sounds
+    }
+  }
+
+  toggleAll(shouldPlay: boolean) {
+    // turn all on or off 
+    const { trackList } = this.state;
+
+    trackList.forEach(track => {
+      shouldPlay ? track.player.play() : track.player.pause();
+    }, this);
+
+    // update state for which one is playing now
+    let updatedTrackList = trackList;
+    updatedTrackList.forEach(track => {
+      track.isPlaying = shouldPlay;
+    }, this);
+    this.setState({
+      trackList: updatedTrackList,
+    })
+  }
+
+  toggleTrack(track: any, index: number) {
+    // update state for which one is playing now
+    const { trackList } = this.state;
+    let updatedTrackList = trackList;
+    updatedTrackList[index].isPlaying = !updatedTrackList[index].isPlaying;
+    this.setState({
+      trackList: updatedTrackList,
+    }, function() {
+      trackList[index].isPlaying ? track.player.play() : track.player.pause()
+    });
+  }
+
+  getlist = () => this.state.trackList.map((item: any, index: number) => {
+    let thisTrack = this.state.trackList[index];
+    return (
+      <div className='playerContainer' key={index}>      
+        <p className='playerTitle'>{item.title}</p>
+        <div 
+          className={`player ${thisTrack.isPlaying ? "isPlaying" : "isNotPlaying"}`}
+          title={item.title}
+        >
+        <FontAwesomeIcon 
+          size="4x" 
+          className="player-icon" 
+          icon={thisTrack.icon} 
+          onClick={() => this.toggleTrack(thisTrack, index) }
+        />
+        {thisTrack.isPlaying && <ReactSlider
+            className="volume-slider"
+            thumbClassName="handle"
+            trackClassName="track"
+            min={0}
+            max={1}
+            step={.1}
+            defaultValue={thisTrack.player.volume || 1}
+            onChange={(volume) => thisTrack.player.volume = volume}
+        />}      
+        <audio 
+          ref={ref => thisTrack.player = ref} 
+          src={thisTrack.src}
+          loop
+          preload="auto"
+        />
+        </div>
+      </div>
+    );
+  });
+
+  getPercentPlaying = ():number => {
+    if (this.state && this.state.trackList) {
+      const numberPlaying = this.state.trackList.filter(x => x.isPlaying).length;
+      const percentPlaying = numberPlaying / this.state.trackList.length * 100;
+      return percentPlaying;
+    }
+    return 0;
+  }
+
+  getPercentAsIndex = () => {
+    return Math.floor(this.getPercentPlaying() / 10);
+  }
+
+  getBackground = () => {
+    return `${backgrounds[this.getPercentAsIndex()]}`;
+  }
+
+  render() {
+
+    return (
+      <>
+        <main style={{ backgroundColor: this.getBackground() }}>
+          <header>
+            <h1>Work-From-Home Soundscapes</h1>
+            <p>
+              Ah, working remotely from a tranquil corner of your home. Is there anything better? Until the noises start, that is. Eliminating those is a key to mastering <a href="https://artofworkingremotely.com/book/" rel="noopener noreferrer" target="_blank">the art of working remotely</a>. Yes, this is a parody of quality ambient noise generators like <a href="https://www.noisli.com/" rel="noopener noreferrer" target="_blank">Noisli</a> and <a href="https://coffitivity.com/" rel="noopener noreferrer" target="_blank">Coffitivity</a>. Stick around if leaf blowers are your jam ... we won't judge!
+            </p>
+            <p><b>Current Status:</b> {this.getPercentPlaying().toFixed(0)}% insanity</p>
+            {this.getPercentPlaying() !== 100 && 
+              <button onClick={() => this.toggleAll(true)}><FontAwesomeIcon 
+                icon={faBomb} 
+              /> Wreck My Flow</button>
+            }
+            {this.getPercentPlaying() !== 0 && 
+              <button onClick={() => this.toggleAll(false)}><FontAwesomeIcon 
+                icon={faDove} 
+              /> Restore My Peace</button>
+            }
+            <p>Did this made you laugh? Give it a share!</p>
+            <div className="share-buttons">
+              <a className="socialShare" href={twitterShareLink} rel="noopener noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faTwitter} /> Tweet
+              </a>
+              <a className="socialShare" href={facebookShareLink} rel="noopener noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faFacebook} /> Share
+              </a>
+            </div>
+          </header>
+          <section className="players">
+            {this.getlist()}
+          </section>
+        </main>
+        <footer>
+          Made with <FontAwesomeIcon 
+            icon={faHeart} 
+            color="red"
+            title="Love"
+          /> by <a href="https://twitter.com/scottpdawson">@scottpdawson</a> and <a href="https://artofworkingremotely.com/book/">The Art of Working Remotely</a>. 
+          Deployed from <a href="https://github.com/scottpdawson/remote-work-soundscapes"><FontAwesomeIcon icon={faGithub} title="Github" /></a> via <a href="https://www.netlify.com/">Netlify</a>.
+        </footer>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/Constants.ts
@@ -0,0 +1,100 @@
+import { 
+    faConciergeBell,
+    faFire,
+    faWarehouse,
+    faSeedling,
+    faTruck,
+    faCoffee,
+    faTree,
+    faCut,
+    faLeaf,
+    faSadTear,
+    faDoorClosed,
+    faDumpster,
+    faHammer,
+    faCar,
+    faMusic,
+    faDog,
+} from '@fortawesome/free-solid-svg-icons'
+  
+export const sounds = [
+    { 
+        title: "Chain Saw",
+        src: 'sounds/chainsaw.mp3',
+        icon: faTree,
+    }, { 
+        title: "Coffee Grinder",
+        src: 'sounds/coffeegrinder.mp3', 
+        icon: faCoffee,
+    }, { 
+        title: "Doorbell",
+        src: 'sounds/doorbell.mp3', 
+        icon: faConciergeBell,
+    }, { 
+        title: "Fire Truck",
+        src: 'sounds/firetruck.wav', 
+        icon: faFire,
+    }, { 
+        title: "Garage Door",
+        src: 'sounds/garagedoor.mp3', 
+        icon: faWarehouse,
+    }, { 
+        title: "Garbage Truck",
+        src: 'sounds/garbagetruck.mp3', 
+        icon: faDumpster,
+    }, { 
+        title: "Lawn Mower",
+        src: 'sounds/lawnmower.mp3', 
+        icon: faCut,
+    }, { 
+        title: "Weedeater",
+        src: 'sounds/weedeater.mp3', 
+        icon: faSeedling,
+    }, { 
+        title: "Mail Truck",
+        src: 'sounds/mailtruck.mp3', 
+        icon: faTruck,
+    }, { 
+        title: "Leaf Blower",
+        src: 'sounds/leafblower.mp3', 
+        icon: faLeaf,
+    }, { 
+        title: "Door Knock",
+        src: 'sounds/flow.mp3', 
+        icon: faDoorClosed,
+    }, { 
+        title: "Construction",
+        src: 'sounds/hammering.mp3', 
+        icon: faHammer,
+    }, { 
+        title: "Piano Practice",
+        src: 'sounds/piano.mp3', 
+        icon: faMusic,
+    }, { 
+        title: "Clarinet Practice",
+        src: 'sounds/clarinet.mp3', 
+        icon: faMusic,
+    }, { 
+        title: "Barking Dog",
+        src: 'sounds/dog.mp3',
+        icon: faDog,
+    }
+];
+
+export const backgrounds = [
+    '#009e51',
+    '#369b37',
+    '#519815',
+    '#699300',
+    '#7f8d00',
+    '#958500',
+    '#ac7b00',
+    '#c26d00',
+    '#d85b00',
+    '#ec4100',
+    '#ff0000',
+];
+
+export const twitterShareLink = `https://twitter.com/share?text=What are the sounds you most enjoy in your work-from-home workspace? Whatever you do, don't wreck your flow. Enjoy these WFH soundscapes from @scottpdawson and @workingrem&url=${window.location.origin}&hashtags=wfh,remotework,soundscapes,leafblowersaremyjam`;
+
+export const facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${window.location.origin}`;
